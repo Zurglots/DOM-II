dac59ad5713a7afb8c2f68474cb11c2c33aec803
Zurglots <bfeole@gmail.com>
2019-05-14 18:02:49 -0400

    scroll, load, focus, keypress added wip

--- README.md
+++ README.md
@@ -34,12 +34,12 @@ Fun Bus wants you to make their site more interactive. They are relying on you t
 ## Task 2: Create Unique Event Listeners
 
 - [ ] Using your [index.js file](js/index.js), create 10 [unique event listeners](https://developer.mozilla.org/en-US/docs/Web/Events). using your creativity to make the Fun Bus site more interactive. Here are some unique events you could try to use:
-      _ [ ] `mouseover`
-      _ [ ] `keydown`
-      _ [ ] `wheel`
+      _ [x] `mouseover`
+      _ [x] `keydown`
+      _ [x] `wheel`
       _ [ ] `drag / drop`
-      _ [ ] `load`
-      _ [ ] `focus`
+      _ [x] `load`
+      _ [x] `focus`
       _ [ ] `resize`
       _ [ ] `scroll`
       _ [ ] `select`
--- js/index.js
+++ js/index.js
@@ -1,1 +1,96 @@
 // Your code goes here
+
+let mainimg = document.querySelector(".intro img");
+
+console.log(mainimg);
+
+// inverse img color on attempt at for EACH img insead of main.
+
+// mainimg.forEach((el, i) => {
+//   const mainimgs = mainimg[i];
+//   el.querySelector("img").addEventListener("mouseover", event => {
+//     event.target.style.filter = "invert(100%)";
+//   });
+//   el.querySelector("img").addEventListener("mouseleave", event => {
+//     event.target.style.filter = "";
+//   });
+// });
+
+// inverse main img
+
+mainimg.addEventListener("mouseover", event => {
+  event.target.style.filter = "invert(100%)";
+});
+
+mainimg.addEventListener("mouseleave", event => {
+  event.target.style.filter = "";
+});
+
+// keypress body
+
+let body = document.querySelector("body");
+console.log(body);
+
+body.addEventListener("keydown", event => {
+  event.target.style.filter = "invert(100%)";
+});
+
+body.addEventListener("keyup", event => {
+  event.target.style.filter = "";
+});
+
+// on scroll effect
+
+let bodyText = document.querySelector(".img-content img");
+console.log(bodyText);
+
+bodyText.addEventListener("wheel", event => {
+  event.target.style.filter = "invert(100%)";
+});
+
+// Need to add toggle?
+
+// on load
+
+window.addEventListener("load", event => {
+  alert("Hey, You're Awesome. Carry On.");
+});
+
+// add focus
+
+const form = document.getElementById("form");
+
+form.addEventListener(
+  "focus",
+  event => {
+    event.target.style.background = "pink";
+  },
+  true
+);
+
+form.addEventListener(
+  "blur",
+  event => {
+    event.target.style.background = "";
+  },
+  true
+);
+
+// resize
+
+// not sure how to implement this
+
+// scroll
+
+const myScroller = document.querySelector("body");
+console.log(myScroller);
+
+myScroller.addEventListener("scroll", event => {
+  event.target.style.background = "red";
+});
+
+// myScroller.forEach(link => {
+//   link.addEventListener("scroll", event => {
+//     event.style.color = "red";
+//   });
+// });
